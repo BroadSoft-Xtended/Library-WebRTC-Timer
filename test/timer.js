fd0ad73060bb130c4d3d78356769465fb85b4808
Dominik Steiner <dominik.j.steiner@gmail.com>
2015-03-23 10:57:50 -0600

    add bdsft-client wrapper to template

--- test/timer.js
+++ test/timer.js
@@ -18,21 +18,22 @@ it('format', function() {
   expect(timerview.text.text()).toEqual( '');
   testUA.startCall();
   expect(timer.text).toEqual( '00:00:00');
+  testUA.endCall();
 });
 it('timer on call started with enableCallTimer = true', function() {
-  testUA.isVisible(timerview.view, false);
+  testUA.isVisible(timerview.timer, false);
   testUA.startCall();
-  testUA.isVisible(timerview.view, true);
+  testUA.isVisible(timerview.timer, true);
   testUA.endCall();
-  testUA.isVisible(timerview.view, false);
+  testUA.isVisible(timerview.timer, false);
   expect(timer.text).toEqual( '00:00:00');
 });
 it('timer on call started with enableCallTimer = false', function() {
   configuration.enableCallTimer = false;
-  testUA.isVisible(timerview.view, false);
+  testUA.isVisible(timerview.timer, false);
   testUA.startCall();
-  testUA.isVisible(timerview.view, false);
+  testUA.isVisible(timerview.timer, false);
   testUA.endCall();
-  testUA.isVisible(timerview.view, false);
+  testUA.isVisible(timerview.timer, false);
 });
 });
